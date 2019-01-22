4d7788218fcfc4d8746ec0ffb0cc5e8beea5b32d
1895718 <1895718@LI-S024-0T03>
2019-01-22 11:12:30 -0500

    afficher Bonjour import de java.util.Scanner

---
+++ Test/src/test/Test.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 1895718
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+               // TODO code application logic here
+        System.out.println("Bonjour");
+        // Demander la saisie du nom de l'utilisateur
+        System.out.println("Veuillez SVP saisir votre nom : ");
+        Scanner readln = new Scanner(System.in);
+        String nom = readln.next();
+        // Affichage du nom de l'utilisateur
+        System.out.println(nom);
+    }
+    
+}
